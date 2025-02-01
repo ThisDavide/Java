fb2143c31cb24c3bfbd2d8c216f69fe08779fa30
ThisDavide <sdavide.policelli@itis.pr.it>
2025-02-01 21:22:48 +0100

    calcolatrice swing

---
+++ CalcolatriceSwing/CalcolatriceSwing/CalcolatriceSwing/Calcolatrice/Calcolatrice/AscoltaBottoni.java
@@ -0,0 +1,211 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class AscoltaBottoni implements ActionListener{
+    public String numCompleto = "0";
+    public String primoNum = "";
+    private String op = "";
+    private String operator = "";
+    private String outString = "";
+    private JLabel outLabel;
+    private double ris = 0;
+    private int divisionePerZero = 0;
+    private double valueOfRis = 0;
+    private int flagOp = 0;
+    private int flagPoint = 0;
+
+    public AscoltaBottoni(JLabel l){
+        this.outLabel = l;
+    }
+
+    public void actionPerformed(ActionEvent e){
+        JButton clickedButton = (JButton)e.getSource();
+        String btnTxt = clickedButton.getText();
+
+        //se viene premuto il tasto C vengono resettate tutte le variabili
+        if(btnTxt.equals("C")){
+            outLabel.setText("0");
+            numCompleto = "";
+            primoNum = "";
+            op = "";
+            outString = "";
+            divisionePerZero = 0;
+            ris = 0;
+            valueOfRis = 0;
+            flagOp = 0;
+            operator = "";
+            flagPoint = 0;
+        }
+        //se premo un operatore
+        else if(btnTxt.equals("+") || btnTxt.equals("-") || btnTxt.equals("x") || btnTxt.equals(":") || btnTxt.equals("x²") || btnTxt.equals("%") || btnTxt.equals("√") || btnTxt.equals("x!") || btnTxt.equals("x10ˣ")){
+            //non permetto di premere più operatori di fila
+            if(flagOp == 0){
+                op = "";
+            }
+            if(!op.equals("+") && !op.equals("-") && !op.equals("x") && !op.equals(":") && !op.equals("x²") && !op.equals("%") && !op.equals("√") && !op.equals("x!") && !op.equals("x10ˣ")){
+                if(outString.equals("0") && numCompleto.equals("")){
+                    outString = "";
+                    outLabel.setText("0");
+                }
+                op = btnTxt;
+                flagOp = 1;
+                outString += op;
+                outLabel.setText(outString);
+                primoNum = numCompleto;
+                numCompleto = "";
+                flagPoint = 0;
+
+                //aggiorno la variabile ris (risultato) in base all'operatore
+                switch(op){ 
+                    case "+":
+                        ris += Double.valueOf(primoNum);
+                        break;
+                    case "-":
+                        if(ris == 0){
+                            ris = Double.valueOf(primoNum) - ris;
+                        }
+                        else{
+                            ris = ris - Double.valueOf(primoNum);
+                        }
+                        break;
+                    case "x":
+                        if(ris == 0){
+                            ris = 1;
+                        }
+                        ris *= Double.valueOf(primoNum);
+                        break;
+                    case ":":
+                        break;
+                    case "x²":
+                        ris = Double.valueOf(primoNum) * Double.valueOf(primoNum);
+                        operator = op;
+                        break;
+                    case "√":
+                        ris = Math.sqrt(Double.valueOf(primoNum));
+                        operator = op;
+                        break;
+                    case "%":
+                        ris = (Double.valueOf(primoNum)) / 100;
+                        operator = op;
+                        break;
+                    case "x!":
+                        double i = 0;
+                        double fattoriale = 1;
+                        for(i = (Double.valueOf(primoNum)); i >= 1; i--){
+                            fattoriale *= i;
+                        }
+                        ris = fattoriale;
+                        operator = op;
+                        break;
+                    case "x10ˣ":
+                        break;
+                    default:
+                        outLabel.setText("Errore");
+                        break;
+                }
+            }
+        }
+        //se ho premuto +/- cambio il segno corrente del numero
+        else if(btnTxt.equals("+/-")){
+            double temp = Double.valueOf(numCompleto) * (-1);
+            numCompleto = String.valueOf(temp);
+            outString = String.valueOf(temp);
+            outLabel.setText(outString);
+        }
+        //se premo il pi greco stampo il suo valore
+        else if(btnTxt.equals("π")){
+            if(!numCompleto.equals(String.valueOf(Math.PI))){
+                double temp = 0;
+                temp = (Math.PI);
+                numCompleto = String.valueOf(temp);
+                outString += String.valueOf(temp);;
+                outLabel.setText(outString);
+            }
+        }
+        //se premo la , 
+        else if(btnTxt.equals(",")){
+            if(!outString.equals(".") && flagPoint != 1){
+                if(outString.equals("")){
+                    outString += 0;
+                }
+                numCompleto += ".";
+                outString += ".";
+                outLabel.setText(outString);
+            }
+            flagPoint = 1;
+        }
+        //se premo l'uguale eseguo il calcolo finale
+        else if(btnTxt.equals("=")){
+            switch(operator){ 
+                case "+":
+                    ris += Double.valueOf(numCompleto);
+                    break;
+                case "-":
+                    ris -= Double.valueOf(numCompleto);
+                    break;
+                case "x":
+                    ris *= Double.valueOf(numCompleto);
+                    break;
+                case ":":
+                    if(Double.valueOf(numCompleto) == 0){
+                        divisionePerZero = 1;
+                    }
+                    else{
+                        ris = Double.valueOf(primoNum) / Double.valueOf(numCompleto); 
+                    }
+                    break;
+                case "x10ˣ":
+                    ris = Double.valueOf(primoNum) * Math.pow(10, Double.valueOf(numCompleto));
+                    break;
+                case "x²":
+                    break;
+                case "√":
+                    break;
+                case "%":
+                    break;
+                case "x!":
+                    break;
+                default:
+                    outLabel.setText("Errore");
+                    break;
+            }
+            //se ho diviso per 0 stampo un messaggio di errore
+            if(divisionePerZero == 1){
+                outLabel.setText("Errore");
+            }
+            //se no stampo il risultato
+            else{
+                outLabel.setText(String.valueOf(ris));
+                //riutilizzo il risultato per il calcolo successivo 
+                primoNum = String.valueOf(ris);
+                outString = String.valueOf(ris);
+            }
+            numCompleto = primoNum;
+            divisionePerZero = 0;   
+            valueOfRis = ris;
+            ris = 0;
+            flagOp = 0;
+            flagPoint = 0;
+        }
+        else{
+            if(outString.equals(String.valueOf(valueOfRis))){
+                numCompleto = "";
+                primoNum = "";
+                op = "";
+                outString = "";
+                divisionePerZero = 0;
+                ris = 0;
+                valueOfRis = 0;
+                flagOp = 0;
+                flagPoint = 0;
+            }
+            numCompleto += btnTxt;
+            outString += btnTxt;
+            outLabel.setText(outString);
+            operator = op;
+            flagOp = 0;
+        } 
+    }
+}
---
+++ CalcolatriceSwing/CalcolatriceSwing/CalcolatriceSwing/Calcolatrice/Calcolatrice/Calcolatrice.java
@@ -0,0 +1,60 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.Font;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.border.LineBorder;
+import javax.swing.*;
+
+public class Calcolatrice extends JFrame{
+    public Calcolatrice(){
+        super();
+        creaCalcolatrice();
+    }
+
+    public void creaCalcolatrice(){
+        String[] arrSimboli = {"x²", "x!", "√", "C", "π", "%", "x10ˣ", "+", "7", "8", "9", "-", "4", "5", "6", "x", "1", "2", "3", ":", "+/-", "0", ",", "="};
+        
+        JPanel panelButtons = new JPanel();
+        this.add(panelButtons, BorderLayout.CENTER);
+        panelButtons.setBackground(Color.BLACK);
+        panelButtons.setLayout(new GridLayout(6, 4));
+
+        JPanel panelResult = new JPanel();
+        this.add(panelResult, BorderLayout.NORTH);
+        panelResult.setPreferredSize(new Dimension(350, 100));
+        panelResult.setBackground(Color.BLACK);
+        
+        JLabel labelResult = new JLabel();
+        labelResult.setText("0");
+        
+        panelResult.add(labelResult);
+        panelResult.setLayout((LayoutManager) new FlowLayout(FlowLayout.RIGHT)); //imposta il testo del pannello allineato a destra
+        labelResult.setForeground(Color.white);
+        labelResult.setFont(new Font("Arial", Font.BOLD, 30));
+        
+        AscoltaBottoni ascoltaBottoni = new AscoltaBottoni(labelResult);
+
+        JButton[] arrButtons = new JButton[24];
+
+        for (int i = 0; i < 24; i++){                        
+			arrButtons[i] = new JButton(arrSimboli[i]); //assegno il simbolo al bottone
+            arrButtons[i].setFont(new Font("Arial", Font.BOLD, 30));
+            arrButtons[i].setBackground(Color.darkGray);
+            arrButtons[i].setForeground(Color.white);
+            arrButtons[i].setBorder(new LineBorder(Color.black, 2));
+            arrButtons[i].addActionListener(ascoltaBottoni);
+            arrButtons[i].setFocusPainted(false);
+			panelButtons.add(arrButtons[i]);
+		}
+        arrButtons[23].setBackground(new Color(130, 190, 250)); //colore tasto =
+        arrButtons[6].setFont(new Font("Arial", Font.BOLD, 25));
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setTitle("Calcolatrice");
+		setSize(350, 500);
+		setVisible(true); 
+    }
+}
